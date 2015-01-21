9f7e17d3f018b6d5d04098ae4f671c980085d66b
Lucas BIGUET-MERMET <lucas.biguetmermet@gmail.com>
2015-01-21 23:58:48 +0100

    design fixes, music + contact section

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,6 @@ DcutWebsite::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.serve_static_assets = true
 end
